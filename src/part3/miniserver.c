b10b15d043c88e78cca09f619abd1bcfcdfd1237
Katheine Evseeva <katherinevse@mail.ru>
2024-03-04 18:19:39 +0300

    -

---
+++ src/part3/miniserver.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcgi_stdio.h>
+
+int main() {
+    while (FCGI_Accept() >= 0) {
+        printf("Status: 200 OK\r\n");
+        printf("Content-type: text/html\r\n\r\n");
+        printf("<html>\n");
+        printf("<head><title>Hello World</title></head>\n");
+        printf("<body>\n");
+        printf("<h1>Hello World!</h1>\n");
+        printf("</body>\n");
+        printf("</html>\n");
+    }
+    return 0;
+}
